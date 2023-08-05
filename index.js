9c21e98bbb31e8bd7006ede7f757c41892f55316
Aakash1049 <80270676+Aakash1049@users.noreply.github.com>
2023-08-05 23:10:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ app.use(cors());
 
 //routes
 app.use( "/users", require("./routes/users")) ;
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('frontend/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
+  }
 
 app.listen(PORT, () => console.log("Server Is Running On Port: " + PORT));
