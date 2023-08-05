254d16e2c77dc334855c3f85c5cf07ba9b581925
Aakash1049 <80270676+Aakash1049@users.noreply.github.com>
2023-08-06 02:58:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ app.use(cors());
 app.use( "/users", require("./routes/users")) ;
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    // app.use(express.static('frontend/build'));
+    app.use(express.static('frontend/build'));
 
-    // app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
-  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
+  // app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+  // app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html')));
 
   }
 
